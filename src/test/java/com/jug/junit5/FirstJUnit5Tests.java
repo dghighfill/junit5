f15114a21b8331675eb2f57f71181e3359a5b111
Dale Highfill <dghighfill@gmail.com>
2017-10-24 18:51:56 -0500

    Added Title Slide

--- src/test/java/com/jug/junit5/FirstJUnit5Tests.java
+++ src/test/java/com/jug/junit5/FirstJUnit5Tests.java
@@ -2,6 +2,9 @@ package com.jug.junit5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
---
+++ src/test/java/com/jug/junit5/TitleSlideTest.java
@@ -0,0 +1,29 @@
+package com.jug.junit5;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TitleSlideTest {
+
+	private List<String> speakers = Arrays.asList("Frank Motley", "Dale Highfill");
+	private int slideCount = 35;
+
+	@Test
+	@DisplayName("Your presenters tonight are")
+	void testForGreatSpeakers() {
+		assertThat(speakers.get(0)).isEqualTo("Frank Motley");
+		assertThat(speakers.get(1)).isEqualTo("Dale Highfill");
+	}
+
+	@Test
+	@DisplayName("Should be a long night")
+	void testHowManySlides() {
+		assertThat(slideCount).isEqualTo(142);
+	}
+
+}
