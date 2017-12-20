a97fd92b3fe8dc81cc4f09c84f59792a2d9cf875
Dale Highfill <dghighfill@gmail.com>
2017-12-20 15:05:43 -0600

    Adding Exercise 4

---
+++ src/main/java/com/jug/junit5/sm/ex4/Ingredient.java
@@ -0,0 +1,30 @@
+package com.jug.junit5.sm.ex4;
+
+public class Ingredient {
+
+	String name;
+	float ounces;
+
+	public Ingredient() {
+	}
+
+	public Ingredient(String name, float ounces) {
+		this.name = name;
+		this.ounces = ounces;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getOunces() {
+		return ounces;
+	}
+
+	public void setOunces(float ounces) {
+		this.ounces = ounces;
+	}
+}
---
+++ src/test/java/com/jug/junit5/sm/ex4/IngredientTest.java
@@ -0,0 +1,101 @@
+package com.jug.junit5.sm.ex4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
+
+@DisplayName("Ingrediant class tests")
+class IngredientTest {
+
+	Ingredient ingredient;
+
+	@BeforeEach
+	void beforeEach() {
+		ingredient = new Ingredient();
+	}
+
+	void testNotNull() {
+		assertThat(ingredient).as("Test that ingredient is created!").isNotNull();
+	}
+
+	@Test
+	void constructWithName() {
+		ingredient = new Ingredient("spinach", 4);
+		assertThat(ingredient.getName()).isEqualTo("spinach");
+		assertThat(ingredient.getOunces()).isEqualTo(4);
+	}
+
+	@Nested
+	@DisplayName("Ingrediant attribute tests")
+	class IngrediantAttributeTest {
+
+		@Test
+		@DisplayName("We should be able to set the name of the ingredient")
+		void testSetName() {
+			ingredient.setName("peanut butter");
+			assertThat(ingredient.getName()).isEqualTo("peanut butter");
+		}
+
+		@Test
+		@DisplayName("We should be able to set the ounces of the ingredient")
+		void testSetOunces() {
+			ingredient.setOunces(16);
+			assertThat(ingredient.getOunces()).isEqualTo(16);
+		}
+
+		@Test
+		@DisplayName("A Test Info")
+		void assertTestInfoWithDiplayName(TestInfo info) {
+			assertThat(info.getDisplayName()).isEqualTo("A Test Info");
+			assertThat(info.getTestMethod().get().getName()).isEqualTo("assertTestInfoWithDiplayName");
+		}
+
+		@Test
+		@DisplayName("Why would I ever do this?")
+		void reportSeveralValues(TestReporter reporter) {
+			ingredient.setName("Key Ingrediant");
+			ingredient.setOunces(16);
+			reporter.publishEntry(ingredient.getName(), Float.toString(ingredient.getOunces()));
+		}
+
+		@RepeatedTest(5)
+		void testCreatingSeveralIngredients(RepetitionInfo info) {
+			System.out.println(String.format("Repeat %d of %d", info.getCurrentRepetition()
+					                                          , info.getTotalRepetitions()));
+		}
+
+		@RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME )
+		void testRepeatAnnotation(TestInfo info) {
+			System.out.println(String.format(info.getDisplayName() ));
+		}
+		
+		@RepeatedTest(value = 1, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+		@DisplayName("Repeat!")
+		void customDisplayName(TestInfo testInfo) {
+			assertThat(testInfo.getDisplayName()).isEqualTo( "Repeat! 1/1");
+		}
+		
+	    @TestFactory
+	    Collection<DynamicTest> dynamicTests() {
+			ingredient.setName("Key Ingredient");
+			ingredient.setOunces(16);
+	        return Arrays.asList(
+	            dynamicTest("dyanmic testSetName", () -> assertThat( ingredient.getName()).isEqualTo("Key Ingredient")),
+	            dynamicTest("dynamic testSetOunces", () -> assertThat( ingredient.getOunces()).isEqualTo( 16 ) ));
+	    }
+	}
+
+}
